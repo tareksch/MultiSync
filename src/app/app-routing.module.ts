f7f5c07cd2e018ebf73f085dbfcb1742ac2126c4
Tarek ahmed <tarek_0@outlook.com>
2022-08-08 16:51:48 +0200

    multisync

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GettingStartedComponent } from './modules/getting-started/getting-started.component';
+import { HomeComponent } from './modules/home/home.component';
+import { PricingComponent } from './modules/pricing/pricing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'getting-started',component:GettingStartedComponent},
+  {path:'pricing',component:PricingComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GettingStartedComponent } from './modules/getting-started/getting-started.component';
+import { HomeComponent } from './modules/home/home.component';
+import { MainComponent } from './modules/main/main.component';
+import { PricingComponent } from './modules/pricing/pricing.component';
+import { GuidesComponent } from './modules/guides/guides/guides.component';
+import { ArchitectureComponent } from './modules/Architecture/architecture/architecture.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GettingStartedComponent,
+    MainComponent,
+    PricingComponent,
+    GuidesComponent,
+    ArchitectureComponent
   ],
   imports: [
     BrowserModule,
